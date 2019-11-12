5d03315449c7ac1bd1a401456a5f44a1e4b43a61
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-11-12 14:27:45 -0800

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <cstring>
 #include "Player.h"
-using namespace std;
 
+Player::player(std::string name, std::string area, int level = 1, int XP = 0)
+{
+  setName(name);
+  setArea(area);
+  setLevel(level);
+  setXP(XP);
+  setMaxHealth();
+  
+}
 //Player Class
