d7d374fb0968401c02161bf4dc26e34767c10e3c
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-11-13 13:41:25 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,5 +5,11 @@
 
 int main()
 {//Main Class
-  
+  char name[80];
+  int option1;
+  std::cout << "What is your name?" << std::endl;
+  std::cin.getline(name, sizeof(name));
+  char location[4] = {"in a hole", "in a cave", "in the mountains", "in a castle"};
+  player account(name, location[0], 1, 0);
+  std::cout << "\nWelcome " << account.getName() << " you find yourself " << account.getArea() << "\nand you are not sure how you ended up here\n" << std::endl;
 }
