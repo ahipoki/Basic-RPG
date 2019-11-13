cab032410d3d3fc36c0f12ad1ad05264dd7bf57e
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-11-13 13:46:07 -0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,10 +1,34 @@
 #include <iostream>
 #include <cstring>
 
-class classPlayer
+class player
 {//Player.h file
 public:
-  player(std::string, std::string, int, int);
+  player(char[], char[], int, int);
+  void setName(char[]);
+  void setArea(char[]);
+  void setLevel(int);
+  void setXP(double);
+  void setHealth(int);
+  void setMaxHealth();
+  void setDamage();
+  char getName();
+  char getArea();
+  int getLevel();
+  int getHealth();
+  int getMaxHealth();
+  int getDamage();
+  int getXP();
+  void setXP(int);
+  int getXPReq();
+  void setXPReq();
 private:
-  std::string playerName;
+  char playerName;
+  char playerArea;
+  int playerLevel;
+  double playerHealth;
+  double playerMaxHealth;
+  int playerDamage;
+  int XP;
+  int XPReq;
 }
